da196c7d19faa840803a0ffadc1195ce7e84e658
Sarvesh_Git <sarveshkohad09@gmail.com>
2026-05-09 09:38:58 +0530

    added logging and custom exception modules

---
+++ src/logger/__init__.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+
+log_dir = "logs"
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+max_log_size = 5 *1024 *1024  # 5MB
+backup_count = 3
+
+log_dir_path = os.path.join(os.getcwd(), log_dir)
+os.makedirs(log_dir_path, exist_ok=True)
+log_file_path = os.path.join(log_dir_path, log_file)
+
+def configure_logger():
+    """
+    Configures logging with a rotating file handler and a console handler.
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s] %(name)s - %(levelname)s - %(message)s')
+
+    # file handler with rotation
+    file_handler = RotatingFileHandler(
+        log_file_path,
+        maxBytes=max_log_size,
+        backupCount=backup_count
+    )
+
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    
+configure_logger()
--- template.py
+++ template.py
@@ -12,6 +12,7 @@ list_of_files = [
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/constants/__init__.py",
+    f"{project_name}/logger/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
